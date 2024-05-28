cf81504dbdb1f5a7648bfe1cd86875f47f1bd431
Leonardo Cabral <leocabrallce@gmail.com>
2024-05-28 09:56:04 +0000

    styling

--- src/app/country/[name]/page.tsx
+++ src/app/country/[name]/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import type { CountryPayload } from "@/types/CountryPayload";
-import type { Country } from "@/types/Country";
 import Image from "next/image";
 
 const API_URL = process.env.API_URL as string;
@@ -20,7 +19,9 @@ async function getCountry(name: string): Promise<CountryPayload> {
   }).then((res) => res.json());
 }
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
   const countryPayload = await getCountry(params.name);
   const country = countryPayload.data;
 
@@ -34,7 +35,6 @@ async function Page({ params }: PageProps) {
   const countryPayload = await getCountry(params.name);
   const country = countryPayload.data;
 
-  // show the country name, capital, flag, population, currency, and language. add the link to the country pages
   return (
     <div className="flex flex-col gap-4 justify-center items-center">
       <div className="max-w-2xl">
@@ -47,23 +47,31 @@ async function Page({ params }: PageProps) {
           ) : null}
           {country.population ? (
             <div>
-              <span className="font-semibold">Population:</span> {country.population}
+              <span className="font-semibold">Population:</span>{" "}
+              {country.population}
             </div>
           ) : null}
           {country.currency ? (
             <div>
-              <span className="font-semibold">Currency:</span> {country.currency}
+              <span className="font-semibold">Currency:</span>{" "}
+              {country.currency}
             </div>
           ) : null}
           {country.description ? (
             <div>
-              <span className="font-semibold">Description:</span> {country.description}
+              <span className="font-semibold">Description:</span>{" "}
+              {country.description}
             </div>
           ) : null}
         </div>
-        {country.href?.flag ?
-          (<Image src={country.href.flag} alt={country.name} width={500} height={300} />) : null
-        }
+        {country.href?.flag ? (
+          <Image
+            src={country.href.flag}
+            alt={country.name}
+            width={500}
+            height={300}
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,17 +19,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header>
-          {/* nav with tailwind */}
           <nav className="bg-gray-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium transition-colors hover:cursor-pointer">
-                        Home
-                      </Link>
-                    </div>
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    <Link
+                      href="/"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium transition-colors hover:cursor-pointer"
+                    >
+                      Home
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -37,9 +37,7 @@ export default function RootLayout({
           </nav>
         </header>
 
-        <div className="py-8 px-4">
-          {children}
-        </div>
+        <div className="py-8 px-4">{children}</div>
       </body>
     </html>
   );
