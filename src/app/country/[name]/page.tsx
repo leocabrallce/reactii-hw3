5c451c5ec2888a192c9b9599ebd4231c8e3c834b
Leonardo Cabral <leocabrallce@gmail.com>
2024-05-28 10:51:59 +0200

    styling

--- src/app/country/[name]/page.tsx
+++ src/app/country/[name]/page.tsx
@@ -34,12 +34,37 @@ async function Page({ params }: PageProps) {
   const countryPayload = await getCountry(params.name);
   const country = countryPayload.data;
 
+  // show the country name, capital, flag, population, currency, and language. add the link to the country pages
   return (
-    <div>
-      <h1>{country.name}</h1>
-      <p>{country.description}</p>
-
-      <Image src={country.href.flag} width={100} height={100} alt={country.name} />
+    <div className="flex flex-col gap-4 justify-center items-center">
+      <div className="max-w-2xl">
+        <h1 className="text-2xl font-semibold text-gray-900">{country.name}</h1>
+        <div className="flex flex-col gap-4">
+          {country.capital ? (
+            <div>
+              <span className="font-semibold">Capital:</span> {country.capital}
+            </div>
+          ) : null}
+          {country.population ? (
+            <div>
+              <span className="font-semibold">Population:</span> {country.population}
+            </div>
+          ) : null}
+          {country.currency ? (
+            <div>
+              <span className="font-semibold">Currency:</span> {country.currency}
+            </div>
+          ) : null}
+          {country.description ? (
+            <div>
+              <span className="font-semibold">Description:</span> {country.description}
+            </div>
+          ) : null}
+        </div>
+        {country.href?.flag ?
+          (<Image src={country.href.flag} alt={country.name} width={500} height={300} />) : null
+        }
+      </div>
     </div>
   );
 }
--- src/components/CountrySelector/CountrySelector.tsx
+++ src/components/CountrySelector/CountrySelector.tsx
@@ -10,29 +10,41 @@ function CountrySelector({ countries }: Props) {
   const [selectedCountry, setSelectedCountry] = useState("");
 
   return (
-    <div>
-      <select
-        value={selectedCountry}
-        onChange={(e) => setSelectedCountry(e.target.value)}
-      >
-        <option value="">Select a country</option>
-        {countries.map((country) => (
-          <option key={country.name} value={country.name}>
-            {country.name}
-          </option>
-        ))}
-      </select>
+    <div className="flex flex-col gap-4 justify-center items-center">
+      <div className="max-w-2xl">
+        <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900">
+          Location
+        </label>
+        <select
+          id="location"
+          name="location"
+          className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          defaultValue=""
+          value={selectedCountry}
+          onChange={(e) => setSelectedCountry(e.target.value)}
+        >
+          <option value="">Select a country</option>
+          {countries.map((country) => (
+            <option key={country.name} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <button
+        disabled={!selectedCountry}
         onClick={() => {
           if (selectedCountry) {
             window.location.href = `/country/${selectedCountry}`;
           }
         }}
+        type="button"
+        className="rounded-md disabled:bg-white bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold disabled:text-gray-900 text-white shadow-sm ring-1 ring-inset ring-indigo-400 disabled:ring-gray-300 hover:bg-indigo-800 hover:cursor-pointer disabled:hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none transition-colors duration-150 ease-in-out"
       >
-        Go
+        Go to country
       </button>
     </div>
   );
 }
 
-export default CountrySelector;
+export default CountrySelector;;
